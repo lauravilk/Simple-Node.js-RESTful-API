d88d28b07764f4a8e4a066e5f7ed67363c27b0c4
lauravilk <la.vilkman@gmail.com>
2026-04-12 13:00:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+// database
+let recepies = [
+    {
+        name: 'Spaghetti',
+        servings: 4,
+        difficulty: 'easy',
+        time: 30
+    },
+    {
+        name: 'Chicken Soup',
+        servings: 2,
+        difficulty: 'hard',
+        time: 45
+    },
+    {
+        name: 'fish tacos',
+        servings: 3,
+        difficulty: 'medium',
+        time: 20
+    }
+]
